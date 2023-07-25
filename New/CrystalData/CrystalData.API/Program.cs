d12cc796382a81246f467b1b8eb251c9e39da3f8
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2023-07-25 15:49:57 +0530

    Added Main for DI

--- New/CrystalData/CrystalData.API/Program.cs
+++ New/CrystalData/CrystalData.API/Program.cs
@@ -2,10 +2,7 @@ using AuthLayer.DataAccess.Impl;
 using AuthLayer.DataAccess.Interface;
 using AuthLayer.Mangers.Impl;
 using AuthLayer.Mangers.Interface;
-using CrystalData.DataAccess.Impl;
-using CrystalData.DataAccess.Interface;
-using CrystalData.Manager.Impl;
-using CrystalData.Manager.Interface;
+using CrystalData.Main;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +13,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-#region Dependency
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddTransient<IUserDataAccess, UserDataAccess>();
 builder.Services.AddTransient<IUserManager, UserManager>();
@@ -25,62 +20,8 @@ builder.Services.AddTransient<IUserManager, UserManager>();
 builder.Services.AddTransient<IRoleDataAccess, RoleDataAccess>();
 builder.Services.AddTransient<IRoleManager, RoleManager>();
 
-builder.Services.AddTransient<ICvAssembliesManager, CvAssembliesManager>();
-builder.Services.AddTransient<ICvAssembliesDataAccess, CvAssembliesDataAccess>();
-
-builder.Services.AddTransient<IAccessManager, AccessManager>();
-builder.Services.AddTransient<IAccessDataAccess, AccessDataAccess>();
-
-builder.Services.AddTransient<IAccessPermissionManager, AccessPermissionManager>();
-builder.Services.AddTransient<IAccessPermissionDataAccess, AccessPermissionDataAccess>();
-
-builder.Services.AddTransient<ICvAssemblyDetailsManager, CvAssemblyDetailsManager>();
-builder.Services.AddTransient<ICvAssemblyDetailsDataAccess, CvAssemblyDetailsDataAccess>();
-
-builder.Services.AddTransient<IAccessPermissionMasterManager, AccessPermissionMasterManager>();
-builder.Services.AddTransient<IAccessPermissionMasterDataAccess, AccessPermissionMasterDataAccess>();
-
-builder.Services.AddTransient<IBranchManager, BranchManager>();
-builder.Services.AddTransient<IBranchDataAccess, BranchDataAccess>();
-
-builder.Services.AddTransient<IBusinessActivityHistoryManager, BusinessActivityHistoryManager>();
-builder.Services.AddTransient<IBusinessActivityHistoryDataAccess, BusinessActivityHistoryDataAccess>();
-
-builder.Services.AddTransient<IBusinessActivityManager, BusinessActivityManager>();
-builder.Services.AddTransient<IBusinessActivityDataAccess, BusinessActivityDataAccess>();
-
-builder.Services.AddTransient<IBusinessActivityRelatedItemsManager, BusinessActivityRelatedItemsManager>();
-builder.Services.AddTransient<IBusinessActivityRelatedItemsDataAccess, BusinessActivityRelatedItemsDataAccess>();
-
-builder.Services.AddTransient<IBusinessActivityTimeAndMaterialsManager, BusinessActivityTimeAndMaterialsManager>();
-builder.Services.AddTransient<IBusinessActivityTimeAndMaterialsDataAccess, BusinessActivityTimeAndMaterialsDataAccess>();
-
-builder.Services.AddTransient<ICarrierManager, CarrierManager>();
-builder.Services.AddTransient<ICarrierDataAccess, CarrierDataAccess>();
-
-builder.Services.AddTransient<ICarrierBillingOptionsManager, CarrierBillingOptionsManager>();
-builder.Services.AddTransient<ICarrierBillingOptionsDataAccess, CarrierBillingOptionsDataAccess>();
-
-builder.Services.AddTransient<ICarrierInsuranceOptionsManager, CarrierInsuranceOptionsManager>();
-builder.Services.AddTransient<ICarrierInsuranceOptionsDataAccess, CarrierInsuranceOptionsDataAccess>();
-
-builder.Services.AddTransient<ICarrierServiceManager, CarrierServiceManager>();
-builder.Services.AddTransient<ICarrierServiceDataAccess, CarrierServiceDataAccess>();
-
-builder.Services.AddTransient<ICarrierVoidOptionsManager, CarrierVoidOptionsManager>();
-builder.Services.AddTransient<ICarrierVoidOptionsDataAccess, CarrierVoidOptionsDataAccess>();
-
-builder.Services.AddTransient<IChargeManager, ChargeManager>();
-builder.Services.AddTransient<IChargeDataAccess, ChargeDataAccess>();
-
-builder.Services.AddTransient<IClassManager, ClassManager>();
-builder.Services.AddTransient<IClassDataAccess, ClassDataAccess>();
-
-builder.Services.AddTransient<IContractTypeManager, ContractTypeManager>();
-builder.Services.AddTransient<IContractTypeDataAccess, ContractTypeDataAccess>();
-
-builder.Services.AddTransient<ICostLayerIssuesManager, CostLayerIssuesManager>();
-builder.Services.AddTransient<ICostLayerIssuesDataAccess, CostLayerIssuesDataAccess>();
+#region Dependency
+Main.AddDependencies(builder.Services);
 #endregion
 
 var app = builder.Build();
---
+++ New/CrystalData/CrystalData.Main/Main.cs
@@ -0,0 +1,75 @@
+﻿using AuthLayer.DataAccess.Interface;
+using CrystalData.DataAccess.Impl;
+using CrystalData.DataAccess.Interface;
+using CrystalData.Manager.Impl;
+using CrystalData.Manager.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrystalData.Main
+{
+    public class Main
+    {
+        public static void AddDependencies(IServiceCollection Service)
+        {
+            Service.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            Service.AddTransient<ICvAssembliesManager, CvAssembliesManager>();
+            Service.AddTransient<ICvAssembliesDataAccess, CvAssembliesDataAccess>();
+
+            Service.AddTransient<IAccessManager, AccessManager>();
+            Service.AddTransient<IAccessDataAccess, AccessDataAccess>();
+
+            Service.AddTransient<IAccessPermissionManager, AccessPermissionManager>();
+            Service.AddTransient<IAccessPermissionDataAccess, AccessPermissionDataAccess>();
+
+            Service.AddTransient<ICvAssemblyDetailsManager, CvAssemblyDetailsManager>();
+            Service.AddTransient<ICvAssemblyDetailsDataAccess, CvAssemblyDetailsDataAccess>();
+
+            Service.AddTransient<IAccessPermissionMasterManager, AccessPermissionMasterManager>();
+            Service.AddTransient<IAccessPermissionMasterDataAccess, AccessPermissionMasterDataAccess>();
+
+            Service.AddTransient<IBranchManager, BranchManager>();
+            Service.AddTransient<IBranchDataAccess, BranchDataAccess>();
+
+            Service.AddTransient<IBusinessActivityHistoryManager, BusinessActivityHistoryManager>();
+            Service.AddTransient<IBusinessActivityHistoryDataAccess, BusinessActivityHistoryDataAccess>();
+
+            Service.AddTransient<IBusinessActivityManager, BusinessActivityManager>();
+            Service.AddTransient<IBusinessActivityDataAccess, BusinessActivityDataAccess>();
+
+            Service.AddTransient<IBusinessActivityRelatedItemsManager, BusinessActivityRelatedItemsManager>();
+            Service.AddTransient<IBusinessActivityRelatedItemsDataAccess, BusinessActivityRelatedItemsDataAccess>();
+
+            Service.AddTransient<IBusinessActivityTimeAndMaterialsManager, BusinessActivityTimeAndMaterialsManager>();
+            Service.AddTransient<IBusinessActivityTimeAndMaterialsDataAccess, BusinessActivityTimeAndMaterialsDataAccess>();
+
+            Service.AddTransient<ICarrierManager, CarrierManager>();
+            Service.AddTransient<ICarrierDataAccess, CarrierDataAccess>();
+
+            Service.AddTransient<ICarrierBillingOptionsManager, CarrierBillingOptionsManager>();
+            Service.AddTransient<ICarrierBillingOptionsDataAccess, CarrierBillingOptionsDataAccess>();
+
+            Service.AddTransient<ICarrierInsuranceOptionsManager, CarrierInsuranceOptionsManager>();
+            Service.AddTransient<ICarrierInsuranceOptionsDataAccess, CarrierInsuranceOptionsDataAccess>();
+
+            Service.AddTransient<ICarrierServiceManager, CarrierServiceManager>();
+            Service.AddTransient<ICarrierServiceDataAccess, CarrierServiceDataAccess>();
+
+            Service.AddTransient<ICarrierVoidOptionsManager, CarrierVoidOptionsManager>();
+            Service.AddTransient<ICarrierVoidOptionsDataAccess, CarrierVoidOptionsDataAccess>();
+
+            Service.AddTransient<IChargeManager, ChargeManager>();
+            Service.AddTransient<IChargeDataAccess, ChargeDataAccess>();
+
+            Service.AddTransient<IClassManager, ClassManager>();
+            Service.AddTransient<IClassDataAccess, ClassDataAccess>();
+
+            Service.AddTransient<IContractTypeManager, ContractTypeManager>();
+            Service.AddTransient<IContractTypeDataAccess, ContractTypeDataAccess>();
+
+            Service.AddTransient<ICostLayerIssuesManager, CostLayerIssuesManager>();
+            Service.AddTransient<ICostLayerIssuesDataAccess, CostLayerIssuesDataAccess>();
+        }
+    }
+}
