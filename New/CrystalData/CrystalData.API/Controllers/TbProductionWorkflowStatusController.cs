bcb1323bd9201110ff2809014ec7045225835b83
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2023-07-27 18:24:56 +0530

    Added ITbProductionWorkflowStatusManager

---
+++ New/CrystalData/CrystalData.API/Controllers/TbProductionWorkflowStatusController.cs
@@ -0,0 +1,82 @@
+﻿using AuthLayer.ActionFilters;
+using AuthLayer.Utility;
+using CrystalData.Manager.Impl;
+using CrystalData.Manager.Interface;
+using CrystalData.Models;
+using EasyCrudLibrary.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CrystalData.API.Controllers
+{
+    [ApiController]
+    [FullAuthorization]
+    public class TbProductionWorkflowStatusController : ControllerBase
+    {
+        ITbProductionWorkflowStatusManager _TbProductionWorkflowStatusManager { get; set; }
+
+        public TbProductionWorkflowStatusController(ITbProductionWorkflowStatusManager TbProductionWorkflowStatusManager)
+        {
+            _TbProductionWorkflowStatusManager = TbProductionWorkflowStatusManager;
+        }
+
+        [HttpPost]
+        [Route("/api/Full/TbProductionWorkflowStatus/Get")]
+        public ActionResult Get(FullGetModel model)
+        {
+            try
+            {
+                if (model.orderBy == null) { model.orderBy = new List<OrderByModel>(); }
+                if (model.filtersList == null) { model.filtersList = new List<AdvanceFilterByModel>(); }
+                return Ok(_TbProductionWorkflowStatusManager.Get(model.page, model.itemsPerPage, model.orderBy, model.filtersList));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new APIResponse(ResponseCode.ERROR, ex.Message, JsonConvert.SerializeObject(ex)));
+            }
+        }
+
+        [HttpPost]
+        [Route("/api/Full/TbProductionWorkflowStatus/Add")]
+        public ActionResult Add(tbProductionWorkflowStatusModel model)
+        {
+            try
+            {
+                return Ok(_TbProductionWorkflowStatusManager.Insert(model));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new APIResponse(ResponseCode.ERROR, ex.Message, JsonConvert.SerializeObject(ex)));
+            }
+        }
+
+        [HttpPost]
+        [Route("/api/Full/TbProductionWorkflowStatus/Update")]
+        public ActionResult Update(Guid GUIDProductionWorkflowStatus, tbProductionWorkflowStatusModel model)
+        {
+            try
+            {
+                return Ok(_TbProductionWorkflowStatusManager.Update(GUIDProductionWorkflowStatus, model));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new APIResponse(ResponseCode.ERROR, ex.Message, JsonConvert.SerializeObject(ex)));
+            }
+        }
+
+        [HttpDelete]
+        [Route("/api/Full/TbProductionWorkflowStatus/HardDelete")]
+        public ActionResult HardDelete(Guid GUIDProductionWorkflowStatus)
+        {
+            try
+            {
+                return Ok(_TbProductionWorkflowStatusManager.HardDelete(GUIDProductionWorkflowStatus));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new APIResponse(ResponseCode.ERROR, ex.Message, JsonConvert.SerializeObject(ex)));
+            }
+        }
+    }
+}
---
+++ New/CrystalData/CrystalData.Models/tbProductionWorkflowStatusModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalData.Models
+{
+    [Table("tbProductionWorkflowStatus")]
+    public class tbProductionWorkflowStatusModel
+    {
+        public Guid GUIDProductionWorkflowStatus { get; set; }
+        public string ProductionWorkFlowStatus { get; set; }
+        public string Description { get; set; }
+        public string Abbreviation { get; set; }
+        public Int32? Sequence { get; set; }
+    }
+}
